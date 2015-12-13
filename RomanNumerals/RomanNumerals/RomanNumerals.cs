64fc39753efb03584fab0a1a2a8e8718b556fe07
Sergio <sergio.villen.galera@hotmail.fr>
2015-12-13 15:40:03 +0100

    Adding algoruthm for 2 & 3. Discovering the pattern

--- RomanNumerals/RomanNumerals/RomanNumerals.cs
+++ RomanNumerals/RomanNumerals/RomanNumerals.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Text;
 
 namespace RomanNumerals
 {
@@ -19,8 +20,16 @@ namespace RomanNumerals
         {
             if (romanNumbers.ContainsKey(arabicNumber)) return romanNumbers[arabicNumber];
 
-            if (arabicNumber == 2) return "II";
-            if (arabicNumber == 3) return "III";
+            if (arabicNumber == 2 || arabicNumber == 3)
+            {
+                var romanNumberBuilder = new StringBuilder();
+                while (arabicNumber > 0)
+                {
+                    romanNumberBuilder.Append("I");
+                    arabicNumber--;
+                }
+                return romanNumberBuilder.ToString();                
+            }            
             if (arabicNumber == 4) return "IV";            
             if (arabicNumber == 6) return "VI";
             if (arabicNumber == 7) return "VII";
